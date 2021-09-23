2e93e61fbd6eb119a961cc59fcd2c7e50e96b570
Jose Antonio Marin Lopez <joseantonio.marinlopez@xploregroup.es>
2021-09-23 16:06:12 +0200

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { TestGeolocationBlockingModule } from 'xplorecomponents';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpartacusModule } from './spartacus/spartacus.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TestGeolocationBlockingModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    SpartacusModule
   ],
   providers: [],
   bootstrap: [AppComponent]
